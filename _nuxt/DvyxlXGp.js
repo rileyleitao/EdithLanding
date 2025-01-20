94a5b767fee771c3fc210c1539acc2e4d67f596e
Riley Parada <rileyleitao@gmail.com>
2025-01-20 00:16:15 -0700

    Updates

--- _nuxt/DvyxlXGp.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,a as _}from"./aPvFMzNO.js";import{_ as a,c as o,b as e,F as s,o as n}from"./CPzVsKj2.js";const r={__name:"Features",setup(c){return(p,m)=>(n(),o(s,null,[e(t),e(_)],64))}},d=a(r,[["__scopeId","data-v-2580223d"]]);export{d as default};
---
+++ _nuxt/XBv5iskl.js
@@ -0,0 +1,1 @@
+import{_ as t,a as _}from"./BzUCN5eJ.js";import{_ as a,c as o,b as e,F as s,o as n}from"./Ct6z6aP3.js";const r={__name:"Features",setup(c){return(p,m)=>(n(),o(s,null,[e(t),e(_)],64))}},d=a(r,[["__scopeId","data-v-2580223d"]]);export{d as default};
